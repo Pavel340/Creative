b754db5b541fa2a4053cd09ebddcf5a9bc929a4e
Pavel340 <zinovev-pasha@mail.ru>
2017-02-27 17:45:11 +0300

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,7 +1,6 @@
 var number = 3; //Количество картинок в папке;
 function imgNext() {
-	var img = document.getElementById('projectImg').src;
-	var numImg = img.slice(87, 88);
+	var numImg = document.getElementById('projectImg').src.slice(-5, -4);
 
 	if((numImg.slice(-1)) == 3) {
 		document.getElementById('projectImg').src = "images/project/img_1.jpg";
@@ -13,10 +12,9 @@ function imgNext() {
 }
 
 function imgBack() {
-	var img = document.getElementById('projectImg').src;
-	var numImg = img.slice(87, 88);
-
-	if((numImg.slice(-1)) == 1) {
+	var numImg = document.getElementById('projectImg').src.slice(-5, -4);
+	
+	if(numImg == '1') {
 		document.getElementById('projectImg').src = "images/project/img_"+number+".jpg";
 	}
 	else {
